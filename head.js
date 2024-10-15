9198f1cfdfa3d030b844de84e32010cde599af27
edgmsh <edgmsh@gmail.com>
2024-10-15 09:39:11 -0400

    new function head is now impleneted

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(inArray) {
+  if (inArray.length === 0 || typeof inArray === "undefined") {
+    return undefined;
+  } else {
+    return inArray[0];
+  }
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head([5, 6, 7]), 9);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
+assertEqual(head([]), "Hello");
+
