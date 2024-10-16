35df70fb1c6edeb8ce7a42f4046c71577b90b31c
edgmsh <edgmsh@gmail.com>
2024-10-16 08:12:00 -0400

    a new assert function for arrays with more detailed results returned

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,21 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    return;
+    return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-        return;
+        return false;
       }
     }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
