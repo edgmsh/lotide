d7f1ab4bbcbea7d65d8591bde80791e9155245a8
edgmsh <edgmsh@gmail.com>
2024-10-16 09:45:41 -0400

    new function without has been implemented

--- without.js
+++ without.js
@@ -22,7 +22,11 @@ const assertArraysEqual = function(array1, array2) {
 const without = function(source, itemsToRemove) {
   let tmpArray = source.slice(0);
   if (tmpArray.length > 0 && typeof itemsToRemove === "string" && tmpArray.includes(itemsToRemove)) {
-    return tmpArray.filter(e => e !== itemsToRemove);
+    for (let i = 0; i < tmpArray.length; i++) {
+      if (tmpArray[i] === itemsToRemove) {
+        tmpArray.splice(i, 1);
+      }
+    }
   } else if (tmpArray.length > 0 && itemsToRemove.length > 0) {
     for (let x = 0; x < itemsToRemove.length; x++) {
       if (tmpArray.includes(itemsToRemove[x])) {
