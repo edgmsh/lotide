60504535ef7630150ade7bceed52e3e1c60a5005
edgmsh <edgmsh@gmail.com>
2024-10-17 11:46:58 -0400

    V2

--- countLetters.js
+++ countLetters.js
@@ -14,7 +14,7 @@ const countLetters = function(inString) {
   } else {
     lettersArr = inString.split(" ").join("").split(''); // spaces removed and converted to Array
     for (let oneLetter of lettersArr) {
-      if (Object.prototype.hasOwnProperty.call(allLetters, oneLetter)) {
+    if (allLetters[oneLetter]) {
         allLetters[oneLetter] += 1;
       } else {
         allLetters[oneLetter] = 1;
@@ -23,8 +23,6 @@ const countLetters = function(inString) {
   }
   return allLetters;
 };
-
-const result1 = countLetters("Hello there");
 
 assertEqual(result1["l"], 2);
 assertEqual(result1["z"], undefined);
