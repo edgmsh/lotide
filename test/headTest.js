87a05b9a113aeda8c1bae054cff03d2e3b4ee4d2
edgmsh <edgmsh@gmail.com>
2024-10-21 19:25:31 -0400

    refactored to run with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,4 @@
+/*
 const head = require("../head");
 const assertEqual = require("../assertEqual");
 
@@ -6,3 +7,16 @@ assertEqual(head([5, 6, 7]), 9);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
 assertEqual(head([]), "Hello");
+*/
+
+const assert = require('assert');
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 5 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 5);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
