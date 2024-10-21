9901ddc4ad44cd63c81826bce27038e4c2b81af9
edgmsh <edgmsh@gmail.com>
2024-10-21 16:40:17 -0400

    refactoed to run with requires

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,7 +19,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
