5b66093e5af179019fcded8608cb47fc9e3276d3
edgmsh <edgmsh@gmail.com>
2024-10-21 15:43:29 -0400

    refactoed to run with requires

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = require("./assertEqual");
+//const assertEqual = require("./assertEqual");
 
 const head = function(inArray) {
   if (typeof inArray === "undefined" || inArray.length === 0) {
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,14 +1,6 @@
 const assertEqual = require("../assertEqual");
-const head = require("../head");
 
-// TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
 assertEqual(1, 5);
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([5, 6, 7]), 9);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
-assertEqual(head([]), "Hello");
