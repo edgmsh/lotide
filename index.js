cbb7b9ba7aa5e7bb4257d56af9f969893202600b
edgmsh <edgmsh@gmail.com>
2024-10-22 13:05:01 -0400

    conveted to mocha

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const countOnly = require('./countOnly');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  eqObjects: eqObjects,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  countOnly: countOnly,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  without: without,
 };
